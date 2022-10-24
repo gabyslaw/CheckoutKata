8cdbf72df320c7d480b23831e38bf7eaf209ffc1
AYOKUNLE OLOWONIWA <AYOKUNLE.OLOWONIWA@sterling.ng>
2022-10-24 19:45:10 +0100

    Added the special price model based on price reduction and minimun number of items

--- CheckoutKata/CheckoutKata/Models/StockItem.cs
+++ CheckoutKata/CheckoutKata/Models/StockItem.cs
@@ -3,7 +3,7 @@
     public class StockItem
     {
         public Dictionary<string, int> newItemPrices { get; set; }
-        public Dictionary<string, string> newSpecialPrice { get; set; }
+        public Dictionary<string, SpecialPrice> newSpecialPrice { get; set; }
 
     }
 }
