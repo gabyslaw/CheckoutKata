efb28bbf6bfbac64733670f90f403d5ff42ba902
AYOKUNLE OLOWONIWA <AYOKUNLE.OLOWONIWA@sterling.ng>
2022-10-24 19:35:15 +0100

    Added the test project

---
+++ CheckoutKata/CheckoutKata.Tests/CheckoutKataTest.cs
@@ -0,0 +1,16 @@
+namespace CheckoutKata.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
